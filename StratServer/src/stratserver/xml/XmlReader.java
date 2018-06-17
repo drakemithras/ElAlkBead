393047c031cb2e94dbab5cb4380ee41cd5ff67ea
drakemithras <drakemithras@gmail.com>
2018-06-17 14:52:17 +0200

    szerver
    Alapvető szerver váz

---
+++ StratServer/src/stratserver/xml/XmlReader.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stratserver.xml;
+
+/**
+ *
+ * @author Ahkriin
+ */
+public class XmlReader {
+    
+}
